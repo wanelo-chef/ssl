d073b4622e21f88faf446d66c823b11eebe734b8
trotttrotttrott <trott@odaacabeef.com>
2014-08-18 15:43:21 -0700

    Use 1.0.1i by default

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['ssl']['openssl']['dependencies'] = %w(
 )
 
 default['ssl']['openssl']['mirror'] = 'https://www.openssl.org/source'
-default['ssl']['openssl']['version'] = '1.0.1h'
-default['ssl']['openssl']['sha1'] = 'b2239599c8bf8f7fc48590a55205c26abe560bf8'
+default['ssl']['openssl']['version'] = '1.0.1i'
+default['ssl']['openssl']['sha1'] = '74eed314fa2c93006df8d26cd9fc630a101abd76'
