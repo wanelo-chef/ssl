5fc520f723d94bf52aefbb73bec52af449b68731
Eric Saxby <eric@wanelo.com>
2015-01-12 14:47:45 -0800

    Update openssl to 1.0.1k

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['ssl']['openssl']['dependencies'] = %w(
 )
 
 default['ssl']['openssl']['mirror'] = 'https://www.openssl.org/source'
-default['ssl']['openssl']['version'] = '1.0.1j'
-default['ssl']['openssl']['sha1'] = 'cff86857507624f0ad42d922bb6f77c4f1c2b819'
+default['ssl']['openssl']['version'] = '1.0.1k'
+default['ssl']['openssl']['sha1'] = '19d818e202558c212a9583fcdaf876995a633ddf'
