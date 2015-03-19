cbbb30466bb158529d29aef02e940ae6bd8a541e
Eric Saxby <sax@livinginthepast.org>
2015-03-19 08:46:01 -0700

    Update openssl to 1.0.1m

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['ssl']['openssl']['dependencies'] = %w(
 )
 
 default['ssl']['openssl']['mirror'] = 'https://www.openssl.org/source'
-default['ssl']['openssl']['version'] = '1.0.1k'
-default['ssl']['openssl']['sha1'] = '19d818e202558c212a9583fcdaf876995a633ddf'
+default['ssl']['openssl']['version'] = '1.0.1m'
+default['ssl']['openssl']['sha1'] = '4ccaf6e505529652f9fdafa01d1d8300bd9f3179'
