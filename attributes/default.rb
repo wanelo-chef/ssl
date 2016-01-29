787516535c5b5e72368e27c5b9c78bb17750a07d
Eric Saxby <sax@livinginthepast.org>
2016-01-28 16:01:32 -0800

    Use openssl 1.0.1r

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,5 +3,5 @@ default['ssl']['openssl']['dependencies'] = %w(
 )
 
 default['ssl']['openssl']['mirror'] = 'https://www.openssl.org/source'
-default['ssl']['openssl']['version'] = '1.0.1p'
-default['ssl']['openssl']['sha1'] = '9d1977cc89242cd11471269ece2ed4650947c046'
+default['ssl']['openssl']['version'] = '1.0.1r'
+default['ssl']['openssl']['sha1'] = 'd2cfa980ef4548da6079fa1e51fe1fb2e5a53e99'
